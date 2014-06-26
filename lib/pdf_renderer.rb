7968c64f569dfb61fe438931aee91027e9e25440
Changlin.Jiang <changlinjiang@gmail.com>
2014-06-26 17:22:45 +0800

    finish

--- lib/pdf_renderer.rb
+++ lib/pdf_renderer.rb
@@ -5,8 +5,13 @@ module PdfRenderer
 
 end
 
-ActionController::Renderers.add :pdf do |filename, options|
+ActionController::Renderers.add :pdf do |obj, options|
+  filename = options[:filename] || 'data'
   pdf = Prawn::Document.new
-  pdf.text render_to_string(options)
+  if obj.respond_to?(:to_pdf) 
+    pdf.text obj.to_pdf
+  else
+    pdf.text obj.to_s
+  end
   send_data(pdf.render, filename: "#{filename}.pdf", disposition: "attachment")
 end
--- test/dummy/app/controllers/home_controller.rb
+++ test/dummy/app/controllers/home_controller.rb
@@ -3,10 +3,8 @@ class HomeController < ApplicationController
   def index
     respond_to  do |format|
       format.html
-      format.pdf { render pdf: 'contents' }
+      format.pdf { render pdf: 'a1', filename: 'contents'}
     end
   end
-
-  
   
 end
